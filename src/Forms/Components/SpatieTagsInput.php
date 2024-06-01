feab61d729b44768a6e55516e1bac23db9007ac1
murphyen <48869596+murphyen@users.noreply.github.com>
2024-06-01 16:57:50 +0100

    Update SpatieTagsInput.php

--- src/Forms/Components/SpatieTagsInput.php
+++ src/Forms/Components/SpatieTagsInput.php
@@ -39,7 +39,7 @@ class SpatieTagsInput extends TagsInput
 //            $component->state($tags->pluck('name')->all());
 //        });
 
-        $this->setState();
+//        $this->setState();
 
         $this->saveRelationshipsUsing(static function (SpatieTagsInput $component, ?Model $record, array $state) {
             if (! (method_exists($record, 'syncTagsWithType') && method_exists($record, 'syncTags'))) {
